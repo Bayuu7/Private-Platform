c5174c47a1566e6af75b041af87a5cdad0c55367
Bayuu7 <103884982+Bayuu7@users.noreply.github.com>
2025-08-09 10:54:20 +0700

    .next.config.js

---
+++ .next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
+    DSRT_API_TOKEN: process.env.DSRT_API_TOKEN,
+    NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE,
+  },
+};
+
+module.exports = nextConfig;
